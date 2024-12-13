f6d2a432ebde8e1953aa5d59f9d975420814cddd
lunguinhoantonio <antoniolunguinho007@gmail.com>
2024-12-13 11:09:20 -0300

    feat: added one more option and starting case 2 WIP: case 2

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,16 @@
 package application;
 import entities.Sorvete;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        ArrayList<Sorvete> sorvetes = new ArrayList<>();
         int quantity;
         System.out.println("1. Adicionar um novo sabor");
         System.out.println("2. Remover um sabor");
+        System.out.println("3. Listar sorvetes");
         switch (scanner.nextInt()) {
             case 1:
                 Sorvete sorvete = new Sorvete();
@@ -21,6 +24,14 @@ public class Main {
                     }
                 } while (quantity < 0);
                 sorvete.setQuantity(quantity);
+                sorvetes.add(sorvete);
+                break;
+            case 2:
+                if (sorvetes.isEmpty()) {
+                    System.out.println("Nenhum sorvete foi cadastrado!");
+                    break;
+                }
+                System.out.println("Qual sorvete deseja remover?");
 
         }
     }
